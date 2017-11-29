87bbd1a2ea9405e51815b0d1c661407c1ea7a891
Josh Fraser <joshfraz@gmail.com>
2017-11-28 23:08:58 -0800

    add routes to whitepaper and product brief

--- views/web_views.py
+++ views/web_views.py
@@ -1,7 +1,16 @@
 from flask import render_template
+from flask import redirect
 
 from app import app
 
 @app.route('/')
 def index():
     return render_template('index.html')
+
+@app.route('/whitepaper')
+def whitepaper():
+    return redirect("/static/docs/whitepaper_v2.pdf", code=302)
+
+@app.route('/product-brief')
+def product_brief():
+    return redirect("/static/docs/product_brief_v15.pdf", code=302)
