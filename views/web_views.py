7aa99989c588559ebc030c63e55fbf4ff8584819
matthewliu <matt@unicyclelabs.com>
2017-11-29 08:20:18 -0800

    Minor change

--- views/web_views.py
+++ views/web_views.py
@@ -13,8 +13,8 @@ def team():
 
 @app.route('/whitepaper')
 def whitepaper():
-    return redirect("/static/docs/whitepaper_v2.pdf", code=302)
+    return redirect('/static/docs/whitepaper_v2.pdf', code=302)
 
 @app.route('/product-brief')
 def product_brief():
-    return redirect("/static/docs/product_brief_v15.pdf", code=302)
+    return redirect('/static/docs/product_brief_v15.pdf', code=302)
