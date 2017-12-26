1c10ad36025dcb2c4a0705945dffeb5da8879092
Josh Fraser <joshfraz@gmail.com>
2017-12-25 23:57:18 -0500

    use flask flashes & alertify.js for website notifications. currently promoting our Telegram and Slack

--- static/js/script.js
+++ static/js/script.js
@@ -1,6 +1,7 @@
 $( "form" ).submit(function( event ) {
   $.post( "/signup?email="+encodeURIComponent($( "#email" ).val()), function( data ) {
     $( "#signup-result" ).html( data );
+    alertify.log(data,"default");
   },"json");
   event.preventDefault();
 });
