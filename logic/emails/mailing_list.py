9fb8ea8331835ea5f6de388d356b9a4f1311cff1
Josh Fraser <joshfraz@gmail.com>
2017-12-27 20:40:16 -0500

    add telegram button to signup email

---
+++ logic/emails/mailing_list.py
@@ -0,0 +1,44 @@
+import re
+
+from flask import jsonify, flash, redirect
+
+from config import universal
+from database import db, db_common, db_models
+from logic.emails import email_types
+from util import sendgrid_wrapper as sgw
+
+DEFAULT_SENDER = sgw.Email(universal.CONTACT_EMAIL, universal.BUSINESS_NAME)
+
+def send_welcome(email, accredited = None, entity_type = None, desired_allocation = None, 
+                    desired_allocation_currency = None, citizenship = None, sending_addr = None, note = None):
+
+    if not re.match(r"(^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$)", email):
+        return 'Please enter a valid email address'
+
+    try:
+        me = db_models.EmailList()
+        me.email = email
+        me.unsubscribed = False
+        db.session.add(me)
+        db.session.commit()
+    except:
+        return 'You are already signed up!'
+
+    to_email = sgw.Email(email, email)
+    email_types.send_email_type('welcome', DEFAULT_SENDER, to_email)
+
+    return 'Thanks for signing up!'
+
+def unsubscribe(email):
+    if not re.match(r"(^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$)", email):
+        return 'Please enter a valid email address'
+
+    try:
+        me = db_models.EmailList.query \
+            .filter(db_models.EmailList.email == email).first()
+        me.unsubscribed = True
+        db.session.commit()
+    except Exception as e:
+        return 'Ooops, something went wrong'
+
+    return 'You have been unsubscribed'
