763da24ea98e7cdfbdfe01b7d05ced543b38cdf9
Stan James <stan@wanderingstan.com>
2018-01-16 20:58:07 -0700

    Logging errors that happen on signup form So they appear in heroku logs

--- logic/emails/mailing_list.py
+++ logic/emails/mailing_list.py
@@ -47,14 +47,15 @@ def presale(full_name, email, accredited, entity_type, desired_allocation, desir
         db.session.add(me)
         db.session.commit()
     except Exception as e:
-         return 'Ooops! Something went wrong.'
+        print (e)
+        return 'Ooops! Something went wrong.'
 
     if sending_addr:
-        sending_addr = "<a href='https://etherscan.io/address/"+sending_addr+"'>"+sending_addr+"</a>" if sending_addr.startswith('0x') else "<a href='https://blockchain.info/address/"+sending_addr+"'>"+sending_addr+"</a>" 
+        sending_addr = "<a href='https://etherscan.io/address/"+sending_addr+"'>"+sending_addr+"</a>" if sending_addr.startswith('0x') else "<a href='https://blockchain.info/address/"+sending_addr+"'>"+sending_addr+"</a>"
 
     message = "Name: %s<br>Email: %s<br>Accredited: %s<br>Entity: %s<br>Desired allocation: %s %s<br>Citizenship: %s<br>Address: %s<br>Note: %s" % (full_name, email,
         ("Yes" if accredited == "1" else "No"), entity_type, desired_allocation, desired_allocation_currency, citizenship, sending_addr, note)
-    
+
     email_types.send_email_type('presale', DEFAULT_SENDER, email)
 
     sgw.notify_admins(message, subject=full_name + " is interested in the presale")
@@ -71,6 +72,7 @@ def unsubscribe(email):
         me.unsubscribed = True
         db.session.commit()
     except Exception as e:
+        print (e)
         return 'Ooops, something went wrong'
 
     return 'You have been unsubscribed'
