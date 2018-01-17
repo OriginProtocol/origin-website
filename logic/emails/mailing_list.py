476e525772f817b9f2ee067407a988b7b30f8af7
Stan James <stan@wanderingstan.com>
2018-01-16 21:16:29 -0700

    Added `gettext()` wrappers to text for email form

--- logic/emails/mailing_list.py
+++ logic/emails/mailing_list.py
@@ -7,13 +7,14 @@ from database import db, db_common, db_models
 from logic.emails import email_types
 from util import sendgrid_wrapper as sgw
 from tools import db_utils
+from flask_babel import gettext, Babel, Locale
 
 DEFAULT_SENDER = sgw.Email(universal.CONTACT_EMAIL, universal.BUSINESS_NAME)
 
 def send_welcome(email):
 
     if not re.match(r"(^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$)", email):
-        return 'Please enter a valid email address'
+        return gettext('Please enter a valid email address')
 
     try:
         me = db_models.EmailList()
@@ -22,16 +23,16 @@ def send_welcome(email):
         db.session.add(me)
         db.session.commit()
     except:
-        return 'You are already signed up!'
+        return gettext('You are already signed up!')
 
     email_types.send_email_type('welcome', DEFAULT_SENDER, email)
 
-    return 'Thanks for signing up!'
+    return gettext('Thanks for signing up!')
 
 def presale(full_name, email, accredited, entity_type, desired_allocation, desired_allocation_currency, citizenship, sending_addr, note):
 
     if not re.match(r"(^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$)", email):
-        return 'Please enter a valid email address'
+        return gettext('Please enter a valid email address')
 
     try:
         me = db_models.Presale()
@@ -47,19 +48,20 @@ def presale(full_name, email, accredited, entity_type, desired_allocation, desir
         db.session.add(me)
         db.session.commit()
     except Exception as e:
-         return 'Ooops! Something went wrong.'
+        print (e)
+        return gettext('Ooops! Something went wrong.')
 
     if sending_addr:
-        sending_addr = "<a href='https://etherscan.io/address/"+sending_addr+"'>"+sending_addr+"</a>" if sending_addr.startswith('0x') else "<a href='https://blockchain.info/address/"+sending_addr+"'>"+sending_addr+"</a>" 
+        sending_addr = "<a href='https://etherscan.io/address/"+sending_addr+"'>"+sending_addr+"</a>" if sending_addr.startswith('0x') else "<a href='https://blockchain.info/address/"+sending_addr+"'>"+sending_addr+"</a>"
 
     message = "Name: %s<br>Email: %s<br>Accredited: %s<br>Entity: %s<br>Desired allocation: %s %s<br>Citizenship: %s<br>Address: %s<br>Note: %s" % (full_name, email,
         ("Yes" if accredited == "1" else "No"), entity_type, desired_allocation, desired_allocation_currency, citizenship, sending_addr, note)
-    
+
     email_types.send_email_type('presale', DEFAULT_SENDER, email)
 
     sgw.notify_admins(message, subject=full_name + " is interested in the presale")
 
-    return 'Thanks! We\'ll be in touch soon.'
+    return gettext('Thanks! We\'ll be in touch soon.')
 
 def unsubscribe(email):
     if not re.match(r"(^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$)", email):
@@ -71,9 +73,10 @@ def unsubscribe(email):
         me.unsubscribed = True
         db.session.commit()
     except Exception as e:
-        return 'Ooops, something went wrong'
+        print (e)
+        return gettext('Ooops, something went wrong')
 
-    return 'You have been unsubscribed'
+    return gettext('You have been unsubscribed')
 
 
 def send_one_off(email_type):
