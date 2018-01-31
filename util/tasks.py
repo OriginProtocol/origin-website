fb7a5fca7acb289c93142851b0da0f127250ccdc
Stan James <stan@wanderingstan.com>
2018-01-31 10:50:07 -0700

    Possible fix for when redis is not running.

--- util/tasks.py
+++ util/tasks.py
@@ -4,28 +4,34 @@ from celery import Celery
 from config import constants
 from flask import Flask
 
-def make_celery(app):
-    celery = Celery(app.import_name, backend=os.environ['REDIS_URL'],
-                    broker=os.environ['REDIS_URL'])
-    celery.conf.update(app.config)
-    TaskBase = celery.Task
-    class ContextTask(TaskBase):
-        abstract = True
-        def __call__(self, *args, **kwargs):
-            with app.app_context():
-                return TaskBase.__call__(self, *args, **kwargs)
-    celery.Task = ContextTask
-    return celery
+if os.environ.get('REDIS_URL') is not None:
 
-flask_app = Flask(__name__)
-flask_app.config.update(
-    CELERY_BROKER_URL=os.environ['REDIS_URL'],
-    CELERY_RESULT_BACKEND=os.environ['REDIS_URL']
-)
-celery = make_celery(flask_app)
+    def make_celery(app):
+        celery = Celery(app.import_name, backend=os.environ['REDIS_URL'],
+                        broker=os.environ['REDIS_URL'])
+        celery.conf.update(app.config)
+        TaskBase = celery.Task
+        class ContextTask(TaskBase):
+            abstract = True
+            def __call__(self, *args, **kwargs):
+                with app.app_context():
+                    return TaskBase.__call__(self, *args, **kwargs)
+        celery.Task = ContextTask
+        return celery
 
-@celery.task()
-def send_email(body):
-    flask_app.logger.fatal("Sending email from Celery...")
-    sg_api = sendgrid.SendGridAPIClient(apikey=constants.SENDGRID_API_KEY)
-    sg_api.client.mail.send.post(request_body=body) 
+        flask_app = Flask(__name__)
+        flask_app.config.update(
+                CELERY_BROKER_URL=os.environ['REDIS_URL'],
+                CELERY_RESULT_BACKEND=os.environ['REDIS_URL']
+        )
+        celery = make_celery(flask_app)
+
+    @celery.task()
+    def send_email(body):
+        flask_app.logger.fatal("Sending email from Celery...")
+        sg_api = sendgrid.SendGridAPIClient(apikey=constants.SENDGRID_API_KEY)
+        sg_api.client.mail.send.post(request_body=body)
+
+else:
+    def send_email(body):
+        print("Redis not installed, so email not sent. Email body:\n%s" % body)
