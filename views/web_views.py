8133662ccbea4ed3ce321194fb48413c0da5c4b1
Stan James <stan@wanderingstan.com>
2018-01-31 12:50:34 -0700

    Whitepaper url fixed to be lang aware and hide version

--- views/web_views.py
+++ views/web_views.py
@@ -55,9 +55,15 @@ def team():
 def presale():
     return render_template('presale.html')
 
-@app.route('/whitepaper')
+@app.route('/<lang_code>/whitepaper')
 def whitepaper():
-    return redirect('/static/docs/whitepaper_v4.pdf', code=302)
+    localized_filename = 'whitepaper_v4_%s.pdf' % g.current_lang.lower()
+    whitepaper_path = (os.path.join(app.root_path, '..', 'static', 'docs', localized_filename))
+    if os.path.isfile(whitepaper_path):
+        return app.send_static_file('docs/%s' % localized_filename)
+    else:
+        # Default to English
+        return app.send_static_file('docs/whitepaper_v4.pdf')
 
 @app.route('/<lang_code>/product-brief')
 def product_brief():
