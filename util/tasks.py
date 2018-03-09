cde96facbcc039f03de6e20a438f562b6edd48b3
Josh Fraser <joshfraz@gmail.com>
2018-03-09 11:20:18 -0800

    Revert "Merge branch 'full-contact-data' of https://github.com/hucares/company-website"
    This reverts commit f62d613e5d1b1c96fd70353269d9da8476c01ac6, reversing
    changes made to f093e0f859ad3700017250c4b6bd4db3206f1612.

--- util/tasks.py
+++ util/tasks.py
@@ -1,34 +1,21 @@
 import os
 import sendgrid
-from app import app_config
 from celery import Celery
 from config import constants
 from flask import Flask
-from numpy.random import randint
-from fullcontact import FullContact
-import logic.emails.mailing_list
-import logic.fullcontact.fullcontact
-
-MIN_RETRY_TIME = 300
-MAX_RETRY_TIME = 600
 
 def make_celery(app):
     celery = Celery(app.import_name, backend=os.environ['REDIS_URL'],
-                    broker=os.environ['REDIS_URL'],
-                    )
+                    broker=os.environ['REDIS_URL'])
     celery.conf.update(app.config)
-
     TaskBase = celery.Task
-
     class ContextTask(TaskBase):
         abstract = True
-
         def __call__(self, *args, **kwargs):
             with app.app_context():
                 return TaskBase.__call__(self, *args, **kwargs)
     celery.Task = ContextTask
     return celery
-
 
 flask_app = Flask(__name__)
 
@@ -38,9 +25,6 @@ flask_app.config.update(
     task_always_eager=os.environ.get('CELERY_DEBUG', False)
 )
 
-
-app_config.init_prod_app(flask_app)
-
 celery = make_celery(flask_app)
 
 
@@ -49,21 +33,3 @@ def send_email(body):
     flask_app.logger.fatal("Sending email from Celery...")
     sg_api = sendgrid.SendGridAPIClient(apikey=constants.SENDGRID_API_KEY)
     sg_api.client.mail.send.post(request_body=body)
-
-
-@celery.task(rate_limit='300/m', max_retries=3)
-def full_contact_request(email):
-    fc = FullContact(constants.FULLCONTACT_KEY)
-    r = fc.person(email=email)
-
-    code = int(r.status_code)
-    if code == 200:
-        logic.fullcontact.fullcontact.fullcontact(email, r.json())
-    elif code == 202:
-        self.retry(countdown=randint(MIN_RETRY_TIME, MAX_RETRY_TIME))
-    elif code == 404:
-        logic.fullcontact.fullcontact.fullcontact(email, r.json())
-    else:
-        flask_app.logger.fatal("FullContact request %s with status code %s",
-                               email, r.status_code)
-        flask_app.logger.fatal(r.json())
