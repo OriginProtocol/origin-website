11615cc51c93bff5656bbd8b33d12d3f22215434
Stan James <stan@wanderingstan.com>
2018-03-15 13:21:51 -0600

    Added check for un-escaped 'at' symbols (@)

--- translations/fix_po_files.py
+++ translations/fix_po_files.py
@@ -24,15 +24,23 @@ for root, dirnames, filenames in os.walk('.'):
         f.seek(0)
         for line in d:
 
-            # Fix spaces in closing tags (Google Translator Toolkit does this)
-            (line, subs) = re.subn(r'</ +', '</', line, flags=re.IGNORECASE)
-            if subs > 0:
-                print ("  -FIXED %s: `</ ` space" % language_code)
+            # Checks only for translation content
+            if line.startswith("msgid") or line.startswith("msgstr"):
 
-            # Fix lone % sybols
-            (line, subs) = re.subn(r'(?<!%)%(?!%)', '%%', line, flags=re.IGNORECASE)
-            if subs > 0:
-                print ("  -FIXED %s: `%%` --> `%%%%`" % language_code)
+                # Fix spaces in closing tags (Google Translator Toolkit does this)
+                (line, subs) = re.subn(r'</ +', '</', line, flags=re.IGNORECASE)
+                if subs > 0:
+                    print ("  -FIXED %s: `</ ` space" % language_code)
+
+                # Fix lone % symbols
+                (line, subs) = re.subn(r'(?<!%)%(?!%)', '%%', line, flags=re.IGNORECASE)
+                if subs > 0:
+                    print ("  -FIXED %s: `%%` --> `%%%%`" % language_code)
+
+                # Fix unescaped @ symbols
+                (line, subs) = re.subn(r'(?<!\\)@', '\\@', line, flags=re.IGNORECASE)
+                if subs > 0:
+                    print ("  -FIXED %s: `@` --> `\\@`" % language_code)
 
             # Fix invalid empty language specification
             if line == '"Language: \\n"\n':
