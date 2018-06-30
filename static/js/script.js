7f1da0b1edf34d7eef94ca83030aca423ac42640
mitchellias <ekemekm@gmail.com>
2018-06-29 17:30:26 -0700

    use correct fa icon and increase spacing

--- static/js/script.js
+++ static/js/script.js
@@ -102,7 +102,7 @@ $(function() {
     var $el, icon, id;
     $el = $(el);
     id = $el.attr('id');
-    icon = '<i class="fa fa-link"></i>';
+    icon = '<i class="fas fa-link"></i>';
     if (id) {
       return $el.prepend($("<a />").addClass("header-link").attr("href", "#" + id).html(icon));
     }
