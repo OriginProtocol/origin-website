1ba1de6445b67018399d8d1ff3ddc160117fba95
Mitch Kessler <ekemekm@gmail.com>
2018-07-03 19:12:52 -0700

    use only css

--- static/js/script.js
+++ static/js/script.js
@@ -95,16 +95,3 @@ $(function() {
 $(function () {
   $('[data-toggle="tooltip"]').tooltip();
 })
-
-// Show header deep link icon on heading hover
-$(function() {
-  return $("h2, h3, h4, h5, h6").each(function(i, el) {
-    var $el, icon, id;
-    $el = $(el);
-    id = $el.attr('id');
-    icon = '<i class="fas fa-link"></i>';
-    if (id) {
-      return $el.prepend($("<a />").addClass("header-link").attr("href", "#" + id).html(icon));
-    }
-  });
-});
