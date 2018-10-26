7e2ac3dad7411c6c30fbea12b481f7f03955be29
natasha08 <natasha08@me.com>
2018-10-26 15:47:06 -0600

    WIP - get follower counts for facebook, reddit, twitter

--- app/app_request.py
+++ app/app_request.py
@@ -1,13 +1,18 @@
+import requests
+
 from requests import get
 from requests.exceptions import RequestException
 from contextlib import closing
 
+headers = {'User-agent': 'Test Bot'}
+
 def simple_get(url):
     try:
-        with closing(get(url, stream=True)) as resp:
+        with closing(get(url, headers=headers, stream=True)) as resp:
             if is_html(resp):
                 return resp.content
             else:
+                print "No html for " + url
                 return None
 
     except RequestException as e:
@@ -15,10 +20,9 @@ def simple_get(url):
         print(error)
         return None
 
-
 def is_html(resp):
+    if resp is None:
+        return False
     content_type = resp.headers['content-type']
 
-    return (resp.status_code == 200
-            and content_type is not None
-            and 'html' in content_type)
+    return (resp.status_code == 200 and 'html' in content_type)
--- app/update_subscribed.py
+++ app/update_subscribed.py
@@ -1,7 +1,26 @@
 from app import app_request
 from bs4 import BeautifulSoup
+import re
 
 sites = []
+
+########### Not working
+
+# sites.append({
+#     'name': 'medium',
+#     'url': 'https://medium.com/originprotocol',
+#     'selector': 'div.u-paddingBottom15',
+# })
+
+# sites.append({
+#     'name': 'instagram',
+#     'url': 'https://www.instagram.com/originprotocol/',
+#     'selector': 'span.g47SY',
+#     'sub_selector': 'title',
+# })
+
+
+######## Working
 
 sites.append({
     'name': 'youtube',
@@ -9,20 +28,67 @@ sites.append({
     'selector': 'span.subscribed',
 })
 
+
+sites.append({
+    'name': 'reddit',
+    'url': 'https://old.reddit.com/r/originprotocol/',
+    'selector': 'span.number',
+})
+
+sites.append({
+    'name': 'twitter',
+    'url': 'https://twitter.com/originprotocol/',
+    'href': '/originprotocol/followers',
+    'selector': 'ProfileNav-value',
+    'sub_selector': 'data-count',
+})
+
+sites.append({
+    'name': 'facebook',
+    'url': 'https://www.facebook.com/originprotocol',
+    'selector': 'clearfix _ikh',
+    'sub_selector': 'div._4bl9 div',
+})
+
+def facebook_user_count(site, html):
+    follower_section = html.find(class_=site['selector'])
+    count_section = follower_section.find(class_='_4bl9')
+    count_with_text = count_section.div.text.encode("ascii")
+    count = int(filter(str.isdigit, count_with_text))
+    site['count'] = count
+
+def instagram_user_count(site, html):
+    follower_section = html.find(class_=site['selector'])
+    count = follower_section['title']
+    site['count'] = count.text.encode("ascii")
+
+def twitter_user_count(site, html):
+    follower_section = html.find(href=site['href'])
+    count = follower_section.find(class_=site['selector'])[site['sub_selector']]
+    site['count'] = count.encode("ascii")
+
 def user_count(site, html):
-    for i, span in enumerate(html.select(site['selector'])):
-        subscribed = html.select(site['selector'])[0].text.encode("ascii")
-        site['count'] = subscribed
-        #save count to database
-        #should just count be saved? Or name, url & selector as well
-        print(site)
-        return True
+    select = html.select(site['selector'])[0]
+    subscribed = select.text.encode("ascii")
+    site['count'] = subscribed
 
 def get_html(site):
     raw_html = app_request.simple_get(site['url'])
-    return BeautifulSoup(raw_html, 'html.parser')
+
+    if raw_html is not None:
+        return BeautifulSoup(raw_html, 'html.parser')
+    else:
+        return ''
 
 for site in sites:
     html = get_html(site)
-    user_count(site, html)
+
+    if site['name'] is 'twitter':
+        twitter_user_count(site, html)
+    elif site['name'] is 'instagram':
+        instagram_user_count(site, html)
+    elif site['name'] is 'facebook':
+        facebook_user_count(site, html)
+    else:
+        user_count(site, html)
     print(sites)
