398caa8a21fe9d01cac765b0054020b2ae3a0faf
Shahul Hameed <shahthepro@outlook.com>
2019-09-11 16:44:58 +0530

    Add investors page

--- views/web_views.py
+++ views/web_views.py
@@ -326,7 +326,8 @@ def assets_all_styles():
         "static/css/pages/product.css",
         "static/css/pages/about.css",
         "static/css/pages/landing.css",
-        "static/css/pages/videos.css"
+        "static/css/pages/videos.css",
+        "static/css/pages/investors.css"
     ]), mimetype="text/css")
 
 @app.route('/static/js/all_javascript.js')
