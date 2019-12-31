726ecee323dae48b7832489b70256cba3ff20d43
Domen Grabec <grabec@gmail.com>
2019-12-31 13:23:26 +0100

    finalize the banner for origin token launch

--- views/web_views.py
+++ views/web_views.py
@@ -481,6 +481,7 @@ def assets_all_styles():
         "static/css/footer.css",
         "static/css/components/countdown-timer.css",
         "static/css/components/countdown-bar.css",
+        "static/css/components/countdown-hero-banner.css",
         "static/css/pages/common.css",
         "static/css/pages/team.css",
         "static/css/pages/token.css",
