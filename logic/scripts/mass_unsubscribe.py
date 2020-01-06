1733d67617ea728d3f6e472d889a532a6163dc67
Franck Chastagnol <franck@originprotocol.com>
2020-01-06 14:55:16 -0800

    mass_unsinscribe.py script: Add option to unsubscribe using DB data as input

--- logic/scripts/mass_unsubscribe.py
+++ logic/scripts/mass_unsubscribe.py
@@ -1,7 +1,8 @@
 # Script for mass unsubscribing emails.
 # Sets the 'unsubscribed' flag in the table email_list and
 # calls SendGrid to remove the email from all mailing lists.
-# Takes as input a csv file having an email on each line.
+# Takes as input either a csv file having an email on each line or reads all rows
+# that are marked as unsubscribed in the email_list table.
 
 import argparse
 import csv
@@ -12,31 +13,34 @@ import traceback
 
 from time import sleep
 
+from database import db_models
 from logic.emails import mailing_list
 from tools import db_utils
 
 BATCH_SIZE = 100
 SLEEP_SEC = 1.0
 
-def process_batch(emails, do_it):
+def process_batch(emails, update_sg, update_db, do_it):
     if not len(emails):
         return True
-    print('%s mode. Unsubscribing %s' % ('Prod' if do_it else 'Dry-run', emails))
+    print('%s mode. Unsubscribing %s Update SG=%s Update DB=%s' % ('Prod' if do_it else 'Dry-run', emails, update_sg, update_db))
     if not do_it:
         return True
     try:
-        # Set the 'unsubscribed' flag on the email_list table.
-        for email in emails:
-            mailing_list.unsubscribe(email)
-        # Call SendGrid to remove the emails from all mailing lists.
-        #mailing_list.mass_unsubscribe_sendgrid_contact(emails)
+        if update_sg:
+            # Call SendGrid to remove the emails from all mailing lists.
+            mailing_list.mass_unsubscribe_sendgrid_contact(emails)
+        if update_db:
+            # Set the 'unsubscribed' flag on the email_list table.
+            for email in emails:
+                mailing_list.unsubscribe(email)
     except Exception as e:
         print('ERROR process_batch:', type(e), e)
         traceback.print_exc()
         return False
     return True
 
-def main(filename, do_it):
+def process_filename(filename, do_it):
     print('Starting mass unsubscribe. Loading data from %s' % filename)
 
     # Init stats.
@@ -66,7 +70,7 @@ def main(filename, do_it):
                 continue
 
             # Process the batch.
-            success = process_batch(emails, do_it)
+            success = process_batch(emails, True, True, do_it)
             if success:
                 num_success += len(emails)
             else:
@@ -78,7 +82,7 @@ def main(filename, do_it):
             sleep(SLEEP_SEC)
 
        # Process the last batch.
-        success = process_batch(emails, do_it)
+        success = process_batch(emails, True, True, do_it)
         if success:
             num_success += len(emails)
         else:
@@ -86,10 +90,65 @@ def main(filename, do_it):
 
     print('Processed %d entries. %s skipped %d successes %d failures' % (num, num_skip, num_success, num_failure))
 
+
+def process_from_db(do_it):
+    print('Starting mass unsubscribe. Loading data from DB')
+
+    # Init stats.
+    num = 0
+    num_skip = 0
+    num_success = 0
+    num_failure = 0
+    emails = []
+
+    # Iterate thru all the entries that are marked as unsubscribed in the DB.
+    EmailList = db_models.EmailList
+    for row in EmailList.query.filter(EmailList.unsubscribed == True).all():
+        num += 1
+        email = row.email
+        if not email or not re.match(r"(^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$)", email):
+            print('Empty or invalid email. Skipping row %d.', num)
+            num_skip += 1
+            continue
+
+        emails.append(email)
+        if len(emails) < BATCH_SIZE:
+            continue
+
+        # Process the batch.
+        success = process_batch(emails, True, False, do_it)
+        if success:
+            num_success += len(emails)
+        else:
+            num_failure += len(emails)
+        print('%d entries processed' % num)
+        emails = []
+
+        # Sleep before processing the next entry for rate limiting purposes.
+        sleep(SLEEP_SEC)
+
+   # Process the last batch.
+    success = process_batch(emails, True, False, do_it)
+    if success:
+        num_success += len(emails)
+    else:
+        num_failure += len(emails)
+
+    print('Processed %d entries. %s skipped %d successes %d failures' % (num, num_skip, num_success, num_failure))
+
+def main(filename, from_db, do_it):
+    if filename:
+        process_filename(filename, do_it)
+    elif from_db:
+        process_from_db(do_it)
+    else:
+        raise Error('Invalid filename or from_db arg')
+
 if __name__ == '__main__':
     with db_utils.request_context():
         parser = argparse.ArgumentParser()
-        parser.add_argument('filename')
+        parser.add_argument('--filename', action='store')
+        parser.add_argument('--from_db', action='store_true')
         parser.add_argument('--do_it', action='store_true')
         args = parser.parse_args()
-        main(args.filename, args.do_it)
+        main(args.filename, args.from_db, args.do_it)
