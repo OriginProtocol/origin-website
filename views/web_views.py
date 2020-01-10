f097e51c81f928403b83d5481462c6edadc29647
Josh Fraser <joshfraz@gmail.com>
2020-01-10 11:52:37 -0800

    redirect product brief link to the new html whitepaper

--- views/web_views.py
+++ views/web_views.py
@@ -159,13 +159,7 @@ def whitepaper():
 @app.route('/product-brief', strict_slashes=False)
 @app.route('/<lang_code>/product-brief', strict_slashes=False)
 def product_brief():
-    localized_filename = 'whitepaper_v18_%s.pdf' % g.current_lang.lower()
-    product_brief_path = (os.path.join(app.root_path, '..', 'static', 'docs', localized_filename))
-    if os.path.isfile(product_brief_path):
-        return app.send_static_file('docs/%s' % localized_filename)
-    else:
-        # Default to English
-        return app.send_static_file('docs/whitepaper_v19.pdf')
+    return redirect('/whitepaper', code=302)
 
 @app.route('/mailing-list/join', methods=['POST'], strict_slashes=False)
 def join_mailing_list():
