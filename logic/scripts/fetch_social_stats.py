9eb9f756936d4ed48847486ada55a17f1c7c077d
Josh Fraser <joshfraz@gmail.com>
2020-01-27 17:01:33 -0800

    track announcements & trading telegram accounts

--- logic/scripts/fetch_social_stats.py
+++ logic/scripts/fetch_social_stats.py
@@ -105,6 +105,20 @@ sites.append({
     'json': False,
 })
 sites.append({
+    'name': 'Telegram (Announcements)',
+    'url': 'https://t.me/originprotocolannouncements',
+    'selector': 'div.tgme_page_extra',
+    'pattern': '([\d\s]+)\s*members',
+    'json': False,
+})
+sites.append({
+    'name': 'Telegram (Trading)',
+    'url': 'https://t.me/ogntrading',
+    'selector': 'div.tgme_page_extra',
+    'pattern': '([\d\s]+)\s*members',
+    'json': False,
+})
+sites.append({
     'name': 'Reddit',
     'url': 'https://old.reddit.com/r/originprotocol/',
     'selector': 'span.number',
