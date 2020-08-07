69214944dec853715ddaa193c70d74172a01ef98
Domen Grabec <grabec@gmail.com>
2020-08-07 19:33:42 +0200

    add mobile styles for landing page

--- static/js/dshop.js
+++ static/js/dshop.js
@@ -4,20 +4,27 @@
     if (scrollImages.length === 0)
       return;
 
-    window.addEventListener('scroll', function(e) {
+    var isMobileDevice = window.innerWidth <= 768
+
+    var scrollFunction = function(e) {
       for (var i = 0; i < scrollImages.length; i++) {
         var image = scrollImages.get(i)
         var rect = image.getBoundingClientRect();
+        var startOffset = isMobileDevice ? 40 : 20;
         var yOffset = 40;
-        var scrollDelta = 80;
+        var scrollDelta = isMobileDevice ? 30 : 80;
+
         var inViewHeight = window.innerHeight + rect.height + scrollDelta;
 
         if (rect.y > -rect.height && rect.y < window.innerHeight + scrollDelta) {
           yOffset = (rect.y + rect.height) / inViewHeight * scrollDelta;
-          image.style.top = `${yOffset + 20}px`;
+          image.style.top = `${yOffset + startOffset}px`;
         }
       }
-    })
+    }
+    
+    scrollFunction()
+    window.addEventListener('scroll', scrollFunction)
   }
 
   document.addEventListener('DOMContentLoaded', onDOMReady)
