0a2c7987f42b41381d3d8f4813acd299491eabee
Domen Grabec <grabec@gmail.com>
2020-09-01 17:34:04 +0200

    allow email list subscriptions to be cross origin

--- views/web_views.py
+++ views/web_views.py
@@ -5,6 +5,7 @@ import re
 import sys
 import calendar
 
+from flask_cors import CORS, cross_origin
 from app import app
 from database import db, db_models
 
@@ -43,6 +44,8 @@ from logic.scripts import token_stats
 # Translation: change path of messages.mo files
 app.config["BABEL_TRANSLATION_DIRECTORIES"] = "../translations"
 babel = Babel(app)
+cors = CORS(app)
+app.config['CORS_HEADERS'] = 'Content-Type'
 
 recaptcha = ReCaptcha(app=app)
 if not recaptcha.is_enabled:
@@ -195,7 +198,7 @@ def whitepaper():
 def product_brief():
     return redirect("/whitepaper", code=302)
 
-
+@cross_origin()
 @app.route("/mailing-list/join", methods=["POST"], strict_slashes=False)
 def join_mailing_list():
     if not "email" in request.form:
