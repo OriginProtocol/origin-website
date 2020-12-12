ec6b869e752cd3cc7d2b98e3b286932c05d826c5
ZaHuPro <itsjakirhussain@gmail.com>
2020-12-13 02:00:04 +0530

    Add stake banner

--- views/web_views.py
+++ views/web_views.py
@@ -614,7 +614,7 @@ def assets_all_styles():
                 "static/css/components/countdown-timer.css",
                 "static/css/components/countdown-bar.css",
                 "static/css/components/countdown-hero-banner.css",
-                "static/css/components/ousd-banner.css",
+                "static/css/components/stake-banner.css",
                 "static/css/pages/common.css",
                 "static/css/pages/team.css",
                 "static/css/pages/token.css",
