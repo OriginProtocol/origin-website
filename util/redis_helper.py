1cb47cf2312208686b0f824fba40ed6c3bc710f4
Shahul Hameed <10547529+shahthepro@users.noreply.github.com>
2023-06-24 05:29:03 +0530

    Try disabling SSL

--- util/redis_helper.py
+++ util/redis_helper.py
@@ -1,8 +1,19 @@
 import os
+from urllib.parse import urlparse
 from redis import Redis, BlockingConnectionPool
+
+url = urlparse(os.environ.get("REDIS_URL"))
 
 def init_redis():
   global redis_client
-  redis_client = Redis(connection_pool=BlockingConnectionPool(max_connections=1)).from_url(os.environ['REDIS_URL'])
+  redis_client = Redis(
+    host=url.hostname,
+    port=url.port,
+    password=url.password,
+    connection_pool=BlockingConnectionPool(max_connections=1),
+    ssl=True, 
+    ssl_cert_reqs=None
+  )
+
 def get_redis_client():
   return redis_client
