15b01f6af79e6d11c18f6e33cfe0c3fc18d3bfa9
Hrik Bhowal <hrik.bhowal@gmail.com>
2023-06-23 16:40:36 -0700

    reenable ssl

--- util/redis_helper.py
+++ util/redis_helper.py
@@ -3,7 +3,6 @@ from redis import Redis, BlockingConnectionPool
 
 def init_redis():
   global redis_client
-  redis_client = Redis(connection_pool=BlockingConnectionPool(max_connections=1), ssl=False, ssl_cert_reqs=None).from_url(os.environ['REDIS_URL'])
-
+  redis_client = Redis(connection_pool=BlockingConnectionPool(max_connections=1)).from_url(os.environ['REDIS_URL'])
 def get_redis_client():
   return redis_client
