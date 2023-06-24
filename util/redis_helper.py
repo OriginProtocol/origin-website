fb3efdd6203612646d942bfd58515db95ca4831e
Shahul Hameed <10547529+shahthepro@users.noreply.github.com>
2023-06-24 05:55:00 +0530

    Revert change to URL

--- util/redis_helper.py
+++ util/redis_helper.py
@@ -1,19 +1,13 @@
 import os
-from urllib.parse import urlparse
 from redis import Redis, BlockingConnectionPool
-
-url = urlparse(os.environ.get("REDIS_URL"))
 
 def init_redis():
   global redis_client
   redis_client = Redis(
-    host=url.hostname,
-    port=url.port,
-    password=url.password,
     connection_pool=BlockingConnectionPool(max_connections=20),
     ssl=True, 
     ssl_cert_reqs=None
-  )
+  ).from_url(os.environ.get("REDIS_URL"))
 
 def get_redis_client():
   return redis_client
